92ff3741daf26c7fef8f91e4a39dc9d30ce8c812
carlos hernandez <chernandezg37@miumg.edu.gt>
2024-10-17 22:22:08 -0600

    se agregaron las rutas de usuario

--- CoworkingPoject/routes/coworking.routes.js
+++ CoworkingPoject/routes/coworking.routes.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const router = express.Router()
 // importar los metodos 
-const {} = require('../controllers/coworking.controller')
+const {getAllUsuarios, addUsuario, getUsuarioPorID, updateUser, deleteUser, putUserRole} = require('../controllers/usuario.controller')
+const {checkRole} = require('../middleware/aouth');
 
 //definir rutas  y métodos de acción
-
+//mostrar todos los usuarios de la aplicacion
+router.get('/usuarios', checkRole ('admin'), getAllUsuarios);
+//Creacion de usuarios
+router.post('/usuarios', addUsuario);
+//Buscar un usuario especifico 
+router.get('/usuarios/:id', getUsuarioPorID);
+//Actualizar un usuario
+router.put('/updateUser/:id', updateUser);
+//Borrar Usuario
+router.delete('/deleteUser/:id', deleteUser);
+//para cambiar si es admin o user
+router.put('/usuarioRol/:id', putUserRole)
 
 
 module.exports = router;
