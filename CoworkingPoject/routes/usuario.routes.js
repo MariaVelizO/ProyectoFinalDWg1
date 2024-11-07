75db5fdac83decba86a1902da619dcdde0f26042
carlos hernandez <chernandezg37@miumg.edu.gt>
2024-11-06 22:09:53 -0600

    correcion de rutas de usuario

--- CoworkingPoject/routes/usuario.routes.js
+++ CoworkingPoject/routes/usuario.routes.js
@@ -7,27 +7,28 @@ const { checkRole } = require('../middleware/auth');
 //definir rutas  y métodos de acción
 
 // registro del usuario
-router.post('/registroUsuario', registroUser );
+router.post('/registerUser', registroUser );
 // inicio de sesion 
-router.post('/login', Login ); 
+router.post('/user-login', Login ); 
 // Ruta para recuperación de contraseña
-router.post('/olvidoContrasena', checkRole(['admin', 'user']), contrasenaOlvidada); 
+router.post('/contrasena-olvidada', checkRole(['admin', 'user']), contrasenaOlvidada); 
 //restablecimiento de la contrasena
-router.post('/resetContrasena', checkRole(['admin', 'user']), restablecerContrasena); 
+router.post('/reestablecer-contrasena', checkRole(['admin', 'user']), restablecerContrasena); 
 // Rutas para manejo de roles
 //router.put('/userRol', putUserRole); 
 // Rutas para Espacios
-router.get('/espaciosDisponibles', checkRole(['user']), getEspaciosDisponibles); 
+router.get('/espacios', checkRole(['user']), getEspaciosDisponibles); 
 //crear una reserva
-router.post('/crearReserva', checkRole(['user']), crearReserva);
+router.post('/reserva', checkRole(['user']), crearReserva);
 //obtener las reservas por usuario
 router.get('/reservas/usuario/:id', checkRole(['user']), reservaPorUsuario); 
 //modificar reserva por usuario
-router.put('/modificarReserva/:id', checkRole(['user']), modifReserva); 
+router.put('/reserva-modificacion/:id', checkRole(['user']), modifReserva); 
 //cancelar reserva por usuario
-router.put('/cancelarReserva/:id', checkRole(['user']), cancelarReserva); 
+router.put('/reserva-cancelacion/:id', checkRole(['user']), cancelarReserva); 
 // Rutas para Comentarios
-router.post('/comentario', checkRole(['user']), agregarComentario); 
+router.post('/comentario-usuario', checkRole(['user']), agregarComentario); 
+
 
 module.exports = router;
 
